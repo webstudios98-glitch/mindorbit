ac0a8afb9c941cd4bfba6b65663b7f0f0729b7ad
webstudios98-glitch <webstudios98@gmail.com>
2025-10-18 18:01:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -165,6 +165,7 @@ def quotesoracle():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+    
 @app.route('/lexilink')
 def lexilink():
     return render_template('lexilink.html')
