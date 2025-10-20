6365e43cb5bb89e5517fab919b26d62bfcd97b98
webstudios98-glitch <webstudios98@gmail.com>
2025-10-20 16:17:12 +0530

    Update lexilink.js

--- static/lexilink.js
+++ static/lexilink.js
@@ -65,9 +65,9 @@
       const y = center + radius * Math.sin(angle);
 
       const node = document.createElement("div");
-      node.className = letter-node ${letter.color};
-      node.style.left = ${x - 32}px;
-      node.style.top = ${y - 32}px;
+      node.className = letter-node `${letter.color}`;
+      node.style.left = `${x - 32}px`;
+      node.style.top = `${y - 32}px`;
       node.dataset.letter = letter.ch;
 
       const inner = document.createElement("div");
